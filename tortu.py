8c62cf94ac0e41b8667c093fc9ed3eecf6974ca4
Susana Tena <susana.tena.r@gmail.com>
2023-09-14 21:23:01 +0200

    creamos un cuadrado

---
+++ tortu.py
@@ -0,0 +1,11 @@
+import turtle
+
+miTortugaMaria = turtle.Turtle()
+miTortugaMaria.forward(50)
+miTortugaMaria.left(90)
+miTortugaMaria.forward(50)
+miTortugaMaria.left(90)
+miTortugaMaria.forward(50)
+miTortugaMaria.left(90)
+miTortugaMaria.forward(50)
+miTortugaMaria.left(90)
